233389986a4f2b7a910557090d9a222ef1e0cea7
Francois Ochoa <fochoa@afwdpe.com>
2019-02-08 12:07:03 -0500

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/models/employee.spec.ts 	new file:   src/app/models/employee.ts 	modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee} from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CRUD';
+
+employeeArray:Employee[] = [
+  { id:1,   name:"Juan",    country: ""},
+  { id:2,   name:"Maria",    country: "peru"},
+  { id:3,   name:"Javier",    country: "venezuela"},
+  { id:4,   name:"Mario",    country: "chile"}
+];
+
+selectedEmployee: Employee = new Employee();
+
+addoredit(){
+  if(this.selectedEmployee.id === 0){
+  this.selectedEmployee.id = this.employeeArray.length+1;
+  this.employeeArray.push(this.selectedEmployee);  
+  }
+  this.selectedEmployee = new Employee();
+}
+
+openforedit(employee:Employee){
+this.selectedEmployee = employee;
+}
+
+delete(){
+  if(confirm('Estas seguro')){
+    this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+    this.selectedEmployee = new Employee();
+  }
+}
+
 }
